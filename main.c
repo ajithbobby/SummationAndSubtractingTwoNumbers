04593e9ad56feb999c1638856e4e12497a30d820
tonysoprano99 <56022534+tonysoprano99@users.noreply.github.com>
2019-10-01 21:30:50 +0530

    Update main.c

--- main.c
+++ main.c
@@ -9,10 +9,13 @@ int main(){
 
     scanf("%d\t%d", &x,&y);
     scanf("%f\t%f", &a,&b);
-
+    printf("Summation of x and y is");
     printf("%d\t", x+y);
+    printf("Difference of x and y is");
     printf("%d\n", x-y);
+    printf("Summation of a and b is");
     printf("%.1f\t", a+b);
+    printf("Difference of a and b is");
     printf("%.1f\n", a-b);
     return 0;
 }
